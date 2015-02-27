56d9445131eddceeb0d96f17bb21525bfd9ec49c
Seonaid <seonaidl@gmail.com>
2015-02-27 13:41:04 -0400

    Added modal drop down to add community on communities tab. Doesn't have back-end connection.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,8 +17,55 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('CommunitiesCtrl', ['$scope', '$http', function($scope, $http) {
-  $scope.communities = [];
+.controller('CommunitiesCtrl', ['$scope', '$http', '$ionicModal',  function($scope, $http, $ionicModal) {
+
+ $scope.communities = [];
+ $scope.newCommunity = function(){
+  $scope.communityModal.show();
+ }
+  // $scope.addCommunity = function() {
+  // var myCommunity = $ionicPopup.show({
+  //   template:'Hey this is going to work!'
+  //   scope: $scope,
+  //   })
+  // }
+
+// Modal for the form
+  // $ionicModal.fromTemplateUrl('my-modal.html', {
+  //   scope: $scope,
+  //   animation: 'slide-in-up'
+  // }).then(function(modal) {
+  //   $scope.modal = modal;
+  // });
+  
+ $ionicModal.fromTemplateUrl('add-community.html', function(modal) {
+    $scope.communityModal = modal;
+  }, {
+    scope: $scope
+  });
+
+  $scope.openModal = function() {
+    $scope.communityModal.show();
+  };
+  
+  $scope.closeModal = function() {
+    $scope.communityModal.hide();
+  };
+  
+  // //Cleanup the modal when we're done with it!
+  // $scope.$on('$destroy', function() {
+  //   $scope.modal.remove();
+  // });
+  
+  // // Execute action on hide modal
+  // $scope.$on('modal.hidden', function() {
+  //   // Execute action
+  // });
+  
+  // // Execute action on remove modal
+  // $scope.$on('modal.removed', function() {
+  //   // Execute action
+  // });
 
   $http.get('http://localhost:3000/api/communities').then(function(response){
     console.log('Success', response);
@@ -28,6 +75,7 @@ angular.module('starter.controllers', [])
   }, function(err){
     console.log('ERR', err);
   })
+
 }])
 
 .controller('ChatsCtrl', function($scope, Chats) {
